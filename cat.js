39f1fab5144c222bfbf0898696f0961bf26ea88d
sonika.tanwani@gmail.com <sonika.tanwani@macys.com>
2016-01-14 13:51:46 -0800

    que 1 solved. changed ruff ruff to mew mew mew

--- cat.js
+++ cat.js
@@ -8,7 +8,7 @@ function Cat (name, breed, age, sound, imgUrl) {
 }
 
 // Cat Instances
-var mrGrumpy = new Cat('Mr.Grumpy', 'persian long hair', 4, '"ruff ruff!"', 'http://www.petwave.com/~/media/Images/Center/Breed/Cats/Semi-Long-and-Long-Hair/Persian/Persian-Cat-Sitting.ashx');
+var mrGrumpy = new Cat('Mr.Grumpy', 'persian long hair', 4, '"mew mew mew!"', 'http://www.petwave.com/~/media/Images/Center/Breed/Cats/Semi-Long-and-Long-Hair/Persian/Persian-Cat-Sitting.ashx');
 var kalie = new Cat('Tiger', 'calico', 0,'"mew mew"','https://s-media-cache-ak0.pinimg.com/236x/08/67/99/0867998a8e927bb50133bb5d2cd25f83.jpg');
 var blackie = new Cat('Blackie','tabby',0, '"meeew"', 'http://placekitten.com/150/170');
 var milo = new Cat('Milo','orange tabby',0, '"moooo"', 'http://oddstuffmagazine.com/wp-content/uploads/2011/09/Small-Cat-580x574.png');
